141f279adc0e84fcb00342539bb95de649b2276e
catonrug <aigars.kadikis@gmail.com>
2021-06-14 21:40:45 +0300

    next

--- 4.0/prometheus-kubernetes/prometheus40.sh
+++ 4.0/prometheus-kubernetes/prometheus40.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# collect data and place it on 'tmp'. host name or IP as a filename
-curl -skL $1://$2:$3/$4 -o /tmp/prometheus.$2.json
+# collect data and place it on 'tmp'. IP address and port will be filename
+curl -skL $1://$2:$3/$4 -o /tmp/prometheus.$2.$3.json
 
 # use JavaScript binary from 5.0 to prepare an LLD JSON
-/var/lib/zabbix/zabbix_js -s /usr/lib/zabbix/externalscripts/prometheus.json.to.lld.js -i /tmp/prometheus.$2.json
-
-# send data to 'Zabbix trapper' item LLD
-# must use -i argument
-
-# send the original JSON to item
-
+zabbix_js -s /usr/lib/zabbix/externalscripts/prometheus.json.to.lld.js -i /tmp/prometheus.$2.$3.json
